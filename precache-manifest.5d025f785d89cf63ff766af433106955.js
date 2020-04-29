d7a006e95f2c9103d886f06a3d5aadbb54048f7e
nextstop-in <shanu801304@gmail.com>
2020-04-29 10:41:50 +0530

    Updates

---
+++ precache-manifest.5d025f785d89cf63ff766af433106955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf45d735d38c9931ca11c006924f5e70",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "5ee4a83eda4156ca077d",
+    "url": "/Monster-Rolodex/static/css/main.eda2891e.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/Monster-Rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee4a83eda4156ca077d",
+    "url": "/Monster-Rolodex/static/js/main.6775b2e5.chunk.js"
+  },
+  {
+    "revision": "2d49b9103f3e45b8c1fb",
+    "url": "/Monster-Rolodex/static/js/runtime-main.2fc03e53.js"
+  }
+]);
